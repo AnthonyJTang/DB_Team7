de319be03a1d3ae0c21c5ba7ada2170d7ead6555
xMattTheRatx15 <38389144+xMattTheRatx15@users.noreply.github.com>
2019-05-03 15:28:54 -0500

    Revert "Revert "Merge branch 'master' of https://github.com/AnthonyJTang/DB_Team7""
    This reverts commit 8aed9f9ab657a96b1672f76188cb91d715b268a7.

--- CreateViews.sql
+++ CreateViews.sql
@@ -1,41 +1,44 @@
---CREATE VIEW TopSalesmen AS 
---    SELECT n.first, n.last, e.date_of_joining
---    FROM EMPLOYEE e, SALESPERSON s, NAMES n
---    WHERE e.EmployeeID = s.EmployeeID
---    AND s.Floor_Sales > 5  or s.Internet_Sales > 5;
+CREATE VIEW TopSalesmen AS 
+    SELECT p.first, p.middle, e.date_of_joining
+    FROM PEOPLE p, EMPLOYEE e, SALESPERSON s
+    WHERE s.Floor_Sales > 5  or s.Internet_Sales > 5;
+    
+SELECT * FROM TOPSALESMEN;     
         
---CREATE VIEW TopCustomer (
---AS
---SELECT fname, lname
---FROM Customer c, BookingDetails b, PaymentTransaction p
---WHERE c.CustomerID = b.CustomerID
---AND b.TransactionID = p.TransactionID
---AND p.amount >  10000);
---
+CREATE VIEW TopCustomer AS
+    SELECT p.first, p.last
+    FROM PEOPLE p, CUSTOMER c, PAYMENTTRANSACTION t, BOOKINGS b
+    GROUP BY b.customerid, p.first, p.last;
+
+SELECT * FROM TOPCUSTOMER;
+
 
 CREATE VIEW PopularDestination AS
-    SELECT bd.Destination, pt.amount
-    FROM BOOKINGDETAIL bd, PAYMENTTRANSACTION pt
-    GROUP BY bd.Destination
-    ORDER BY COUNT(GROUP BY bd.Destination) DESC;
+    SELECT b.Destination, pt.amount
+    FROM BOOKINGS b, PAYMENTTRANSACTION pt
+    GROUP BY b.Destination, pt.amount
+    ORDER BY COUNT(GROUP(b.Destination));
+    
+SELECT * FROM POPULARDESTINATION;
 
-SELECT * FROM POPULARDESTINATION
 
+CREATE VIEW PotentialMember AS
+    SELECT p.first, p.last, pn.PhoneNumber
+    FROM PEOPLE p, PHONENUMBER pn, CUSTOMER c
+    WHERE c.MemberShipID = null;
+    
+SELECT * FROM POTENTIALMEMBER;
 
---CREATE VIEW PotentialMember(
---AS
---SELECT Name, PhoneNumber
---WHERE
---    COUNT( b.CustomerID = c.MembershipID) > 3
---FROM Customer c, Phone ph, Bookings b
---WHEN c.MembershipID NOT NULL);
---
---CREATE VIEW PopularPromotion(
---AS
---SELECT Description
---WHEN 
---    COUNT(PromotionID)
---FROM Promotion
---GROUP BY PromotionID
---ORDER BY value_occurrence DESC
---LIMIT    1);
+CREATE VIEW PopularPromotion AS
+    SELECT b.destination, pt.PromotionID
+    FROM PaymentTransaction pt, BOOKINGS b
+    GROUP BY pt.PromotionID, b.destination
+    ORDER BY COUNT(GROUP(b.destination));
+    
+SELECT * FROM POPULARPROMOTION;    
+
+DROP VIEW TOPSALESMEN;
+DROP VIEW TOPCUSTOMER;
+DROP VIEW POPULARDESTINATION;
+DROP VIEW POTENTIALMEMBER;
+DROP VIEW POPULARPROMOTION;
